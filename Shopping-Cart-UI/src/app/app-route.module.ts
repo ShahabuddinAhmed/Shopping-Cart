83f1b17061a607fa57a0b32d2a57b72f86b6ce9d
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-11 10:56:44 +0600

    app routing module is created

---
+++ Shopping-Cart-UI/src/app/app-route.module.ts
@@ -0,0 +1,20 @@
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRouteModule { }
